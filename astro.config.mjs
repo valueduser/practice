1d783b50f757281d747116c26fec9693ea937402
Chris Lowe <clowe@luc.edu>
2025-05-28 21:42:23 -0600

    add sentry, update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
+import sentry from '@sentry/astro'
 import tailwind from '@astrojs/tailwind'
 
 import node from '@astrojs/node'
@@ -8,7 +8,19 @@ import 'dotenv/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sentry({
+      dsn: 'https://05308ed7adf7eec9d9098ac7704ba244@o141824.ingest.us.sentry.io/4509403298332672',
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      sourceMapsUploadOptions: {
+        project: 'practice',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   output: 'server',
 
   adapter: node({
